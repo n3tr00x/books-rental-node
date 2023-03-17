56ae35962dcde5a73afce5f7cbb74b895c1679ae
Hubert Warchoł <n3tr00x99@gmail.com>
2023-03-17 23:43:41 +0100

    create method which renders the menu

--- App/App.js
+++ App/App.js
@@ -1,6 +1,16 @@
 class App {
+	#displayMenu() {
+		const header = 'Witaj w wypożyczani książek!\n\n';
+		const menu = 'Menu:\n';
+		const options =
+			'1. Dodaj wypożyczenie\n2. Wyświetl listę wypożyczeń\n3. Wykonanie obliczeń\n4. Zapis do pliku\n';
+		const exit = '5. Wyjdź z programu';
+
+		return header + menu + options + exit;
+	}
+
 	main() {
-		console.log('Hello, world!');
+		console.log(this.#displayMenu());
 	}
 }
 
