7d3b696f4107b3327f2bef5e9bcb0ecce9dd7a9a
Hubert Warchoł <n3tr00x99@gmail.com>
2023-03-18 19:26:39 +0100

    create a simple database and a helpful function for read data from it

--- utils.js
+++ utils.js
@@ -1,8 +1,20 @@
 import PromptSync from 'prompt-sync';
 import chalk from 'chalk';
+import { readFileSync } from 'fs';
+
+const databasePath = './database/database.json';
 
 export const input = PromptSync();
 export const { log, clear } = console;
 export const error = message => {
 	log(chalk.red(message));
+};
+
+export const readData = () => {
+	try {
+		const data = readFileSync(databasePath);
+		return JSON.parse(data);
+	} catch (err) {
+		error('Błąd: ' + err.message);
+	}
 };
