892d56d53e08b71a5daaa2cad3116012b64a0e4f
Hubert Warchoł <n3tr00x99@gmail.com>
2023-03-18 19:27:44 +0100

    rebuild the menu

--- App/App.js
+++ App/App.js
@@ -1,16 +1,59 @@
+import { readData, clear, error, input, log as print } from '../utils.js';
+
 class App {
+	#greetings() {
+		return 'Witaj w wypożyczani książek!\n';
+	}
+
 	#displayMenu() {
-		const header = 'Witaj w wypożyczani książek!\n\n';
 		const menu = 'Menu:\n';
 		const options =
-			'1. Dodaj wypożyczenie\n2. Wyświetl listę wypożyczeń\n3. Wykonanie obliczeń\n4. Zapis do pliku\n';
-		const exit = '5. Wyjdź z programu';
+			'(1) Dodaj wypożyczenie\n(2) Wyświetl listę wypożyczeń\n(3) Wykonanie obliczeń\n(4) Zapis do pliku\n';
+		const exit = '(5) Wyjdź z programu';
 
-		return header + menu + options + exit;
+		return menu + options + exit;
+	}
+
+	#menuHandler(option) {
+		switch (option) {
+			case '1':
+				clear();
+				this.#applicationHandler();
+				break;
+			case '2':
+				clear();
+				this.#displayRentals();
+				this.#applicationHandler();
+				break;
+			case '5':
+				clear();
+				print('Dziękujemy za skorzystanie z programu :)');
+				break;
+			default:
+				clear();
+				error('Wprowadzono złą opcję w menu!');
+				this.#applicationHandler();
+				break;
+		}
+	}
+
+	#applicationHandler() {
+		//displaying menu
+		print(this.#displayMenu());
+		//asking a option menu
+		const option = input('Wybierz opcję: ');
+		this.#menuHandler(option);
+	}
+
+	#displayRentals() {
+		console.log(readData());
 	}
 
 	main() {
-		console.log(this.#displayMenu());
+		clear();
+		//greetings
+		print(this.#greetings());
+		this.#applicationHandler();
 	}
 }
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import App from './App/App.js';
+import App from './app/App.js';
 
 const app = new App();
 app.main();
