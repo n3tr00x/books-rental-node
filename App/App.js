a5ac57cf91bde13e65f989bdf0045c3744328efa
Hubert Warchoł <n3tr00x99@gmail.com>
2023-03-22 01:34:54 +0100

    make the functions for rentals things

--- App/App.js
+++ App/App.js
@@ -5,22 +5,14 @@ import {
 	input,
 	log as print,
 	writeData,
+	validate,
 } from '../utils.js';
-import Book from '../Domain/Book/Book.js';
+import Menu from './Menu/Menu.js';
+import Book from '../Domain/Book.js';
+import BoardGame from '../Domain/BoardGame.js';
 
 class App {
-	#greetings() {
-		return 'Witaj w wypożyczani książek!\n';
-	}
-
-	#displayMenu() {
-		const menu = 'Menu:\n';
-		const options =
-			'(1) Dodaj wypożyczenie\n(2) Wyświetl listę wypożyczeń\n(3) Wykonanie obliczeń\n(4) Zapis do pliku\n';
-		const exit = '(5) Wyjdź z programu';
-
-		return menu + options + exit;
-	}
+	menu = new Menu();
 
 	#menuHandler(option) {
 		switch (option) {
@@ -47,24 +39,33 @@ class App {
 
 	#applicationHandler() {
 		//displaying menu
-		print(this.#displayMenu());
+		print(this.menu.displayMenu());
 		//asking a option menu
 		const option = input('Wybierz opcję: ');
 		this.#menuHandler(option);
 	}
 
 	#displayRentals() {
-		console.log(readData());
+		print(readData());
 	}
 
 	#addBookRental() {
 		clear();
-		const name = input('Podaj nazwę: ');
-		const publisher = input('Podaj wydawcę: ');
-		const amount = input('Podaj kwotę: ');
-		const author = input('Podaj autora: ');
-		// print(Book.getTypes());
-		const type = input('Podaj rodzaj książki: ');
+
+		const texts = {
+			name: 'Podaj nazwę: ',
+			publisher: 'Podaj wydawcę: ',
+			amount: 'Podaj kwotę: ',
+			author: 'Podaj autora: ',
+			type: 'Podaj rodzaj książki: ',
+		};
+
+		const name = validate(texts.name, 'string');
+		const publisher = validate(texts.publisher, 'string');
+		const amount = validate(texts.amount, 'number');
+		const author = validate(texts.author, 'string');
+		print(Book.getTypes());
+		const type = validate(texts.type, 'number');
 
 		const book = new Book(name, publisher, amount, author, type);
 
@@ -76,14 +77,50 @@ class App {
 			type: book.getType(),
 		};
 
-		// print(data);
+		console.log(data);
 
-		writeData(data);
+		// writeData(data);
+	}
+
+	#addBoardGameRental() {
+		clear();
+
+		const texts = {
+			name: 'Podaj nazwę: ',
+			publisher: 'Podaj wydawcę: ',
+			amount: 'Podaj kwotę: ',
+			minAge: 'Podaj minimalny wiek: ',
+			maxAge: 'Podaj maksymalny wiek: ',
+		};
+
+		const name = validate(texts.name, 'string');
+		const publisher = validate(texts.publisher, 'string');
+		const amount = validate(texts.amount, 'number');
+		const minAge = validate(texts.minAge, 'number');
+		const maxAge = validate(texts.maxAge, 'number');
+
+		const boardGame = new BoardGame(
+			name,
+			publisher,
+			amount,
+			minAge,
+			maxAge
+		);
+
+		const data = {
+			name: boardGame.getName(),
+			publisher: boardGame.getPublisher(),
+			amount: boardGame.getAmount(),
+			minAge: boardGame.getMinAge(),
+			maxAge: boardGame.getMaxAge(),
+		};
+
+		console.log(data);
+
+		// writeData(data);
 	}
 
 	#addRentalHandler() {
-		const data = readData();
-
 		print('(1) Książka\n(2) Gra planszowa');
 		const rentalType = input('Podaj typ pozycji: ');
 
@@ -92,6 +129,7 @@ class App {
 				this.#addBookRental();
 				break;
 			case '2':
+				this.#addBoardGameRental();
 				break;
 			default:
 				clear();
@@ -103,7 +141,7 @@ class App {
 	main() {
 		clear();
 		//greetings
-		print(this.#greetings());
+		print(this.menu.greetings());
 		this.#applicationHandler();
 	}
 }
