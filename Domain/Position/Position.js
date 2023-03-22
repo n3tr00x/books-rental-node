d53a4ec77ca95aef127ec0d264110ea7193d2975
Hubert Warchoł <n3tr00x99@gmail.com>
2023-03-22 18:20:45 +0100

    correct the id pole and create the class name pole

--- Domain/Position/Position.js
+++ Domain/Position/Position.js
@@ -1,13 +1,24 @@
 class Position {
+	#id;
+	#className;
 	#name;
 	#publisher;
 	#amount;
 
 	constructor(name, publisher, amount) {
-		this.id = Date.now();
+		this.#id = Date.now();
+		this.#className = this.constructor.name.toLowerCase();
 		this.#name = name;
 		this.#amount = amount;
 		this.#publisher = publisher;
+	}
+
+	getId() {
+		return this.#id;
+	}
+
+	getClassName() {
+		return this.#className;
 	}
 
 	getName() {
