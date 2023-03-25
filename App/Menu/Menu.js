daebb116a72e21bc143c5cf8c198adbd561cc144
Hubert Warchoł <n3tr00x99@gmail.com>
2023-03-25 15:31:16 +0100

    remove write to file option from menu

--- App/Menu/Menu.js
+++ App/Menu/Menu.js
@@ -6,8 +6,8 @@ class Menu {
 	displayMenu() {
 		const menu = 'Menu:\n';
 		const options =
-			'(1) Dodaj wypożyczenie\n(2) Wyświetl listę wypożyczeń\n(3) Wykonanie obliczeń\n(4) Zapis do pliku\n';
-		const exit = '(5) Wyjdź z programu';
+			'(1) Dodaj wypożyczenie\n(2) Wyświetl listę wypożyczeń\n(3) Wykonanie obliczeń\n';
+		const exit = '(4) Wyjdź z programu';
 
 		return menu + options + exit;
 	}
